faa75579d69044eb8ccff2ede3e233c06ea0100b
Maxim <334890@gmail.com>
2023-08-01 11:27:45 +0600

    add theme colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import "./App.css";
+import { useState } from "react";
+import personIcon from "./assets/person_icon.svg";
 
-function App() {
-  return <></>;
+export default function App() {
+  const [theme, setTheme] = useState("DARK");
+
+  return (
+    <div
+      className={`app-container ${
+        theme === "DARK" ? "dark-theme" : "light-theme"
+      }`}
+    >
+      <header className="header">
+        <button
+          className="theme-toggle"
+          onClick={() => setTheme(theme === "DARK" ? "LIGHT" : "DARK")}
+        >
+          theme
+        </button>
+
+        <img className="icon" src={personIcon} alt="avatar icon" />
+        <nav className="navigation"></nav>
+      </header>
+      <main className="content">main</main>
+      <footer className="footer">footer</footer>
+    </div>
+  );
 }
-
-export default App;
