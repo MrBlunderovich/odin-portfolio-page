89346d10932bdba63bc202ecd062836edb8d11b8
Maxim <334890@gmail.com>
2023-08-04 12:02:31 +0600

    add links and icons to cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,28 @@
+import GitHubIcon from "../UI/GitHubIcon";
+import OpenInNewIcon from "../UI/OpenInNewIcon";
 import styles from "./Card.module.css";
+
 /** @typedef {import('../projects.js').Projects} Projects */
 
 export default function Card({ children }) {
-  const { title, screenshot: imageUrl, description } = children;
+  const {
+    title,
+    screenshot: imageUrl,
+    description,
+    source,
+    deployment,
+  } = children;
   return (
     <article className={styles.card}>
-      <h2>{title}</h2>
+      <div className={`${styles["heading-container"]}`}>
+        <h2>{title}</h2>
+        <a href={source} aria-label="github source code">
+          <GitHubIcon className={styles.icon} />
+        </a>
+        <a href={deployment} aria-label="live deployment">
+          <OpenInNewIcon className={styles.icon} />
+        </a>
+      </div>
       <img src={imageUrl} alt="app screenshot" width="100%" />
       <p className="description">{description}</p>
     </article>
--- src/projects.js
+++ src/projects.js
@@ -16,6 +16,7 @@ const projects = [
     title: "Battleship game",
     screenshot: new URL("/screenshots/battleship.png", import.meta.url).href,
     source: "https://github.com/MrBlunderovich/odin-battleship",
+    deployment: "https://mrblunderovich.github.io/odin-battleship/",
     description:
       "A game with moderately intelligent algorithm for a computer player. Built in TDD style in mind.",
   },
@@ -24,6 +25,7 @@ const projects = [
     title: "Knight's travails",
     screenshot: new URL("/screenshots/knights.png", import.meta.url).href,
     source: "https://github.com/MrBlunderovich/odin-knights-travails",
+    deployment: "https://mrblunderovich.github.io/odin-knights-travails/",
     description:
       "A visualization of a breadth-first graph search of a short path for a chess knight.",
   },
@@ -32,6 +34,7 @@ const projects = [
     title: "Weather app",
     screenshot: new URL("/screenshots/weather.png", import.meta.url).href,
     source: "https://github.com/MrBlunderovich/odin-weather",
+    deployment: "https://mrblunderovich.github.io/odin-weather",
     description:
       "An app that loads data from weatherapi.com and displays it in a couple of ways including windguru.cz-style three day tabular forecast.",
   },
@@ -40,6 +43,7 @@ const projects = [
     title: "Calendar",
     screenshot: new URL("/screenshots/calendar.png", import.meta.url).href,
     source: "https://github.com/MrBlunderovich/calendar",
+    deployment: "https://mrblunderovich.github.io/calendar",
     description:
       "My early experience with React components. A circular calendar representing a year as I see it in my imagination. It is also meant to draw attention to the velocity of time passing.",
   },
